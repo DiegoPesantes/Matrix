a50a532cb57da1e7ff77fd8cbfa9fd388cfd1fd5
Laboratorio <laboratorio@udla-ec.int>
2023-11-21 14:57:13 -0500

    Pide datos a los usuarios

---
+++ Matriz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int n, m;
+    printf("Ingrese el numero de filas");
+    scanf("%d", &n);
+    printf("Ingrese el numero de columnas");
+    scanf("%d", &m);
+
+    
+    return 0;
+}
